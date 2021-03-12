36a70c009e5e43567ca662aac43c8de605d244c7
francodvelasco <58771266+francodvelasco@users.noreply.github.com>
2021-03-13 01:12:57 +0800

    refactored personal website to be cleaner

---
+++ headerScript.js
@@ -0,0 +1,4 @@
+console.log("Hey there, sneaky!");
+console.log("You seem like the type who's interested in my code.");
+console.log("If you are, perhaps you'd be interested at looking at my other code creations!");
+console.log("Find them at https://github.com/francodvelasco (though it's more of a code dump if anything)");
